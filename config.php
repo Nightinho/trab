2cbed84c5f5f8fd4105d56cb7a173a27be9e9608
Nightinho <nightmarejockernight@gmail.com>
2024-09-24 17:23:12 -0300

    quarto commit

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+    $dbhost = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "registro_odisseia";
+
+    $conexão = new mysqli($dbhost,$dbUsername,$dbPassword,$dbName);
+    //if($conexao->connect_errno)
+    //{
+    //    echo "Erro";
+    //}
+    //else
+    //{
+    //    echo "Conexão efetuada com sucesso";
+    //}
+
+?>
